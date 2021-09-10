bbb66fffd1d077c7b67872807f1891ad295c3a08
EddyBautista <bautista.j.eddy@gmail.com>
2021-09-10 06:16:57 -0500

    build out state class and update build

--- lib/main.dart
+++ lib/main.dart
@@ -7,8 +7,7 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // This class creates the instance of a widget app
-    final wordPair = WordPair.random(); // set the random word var at the top 
+    // This class creates the instance of a widget app 
     return MaterialApp(
       title: "Welcome to Flutter", // identify the app for the user 
       home: Scaffold( // visual scaffold
@@ -16,7 +15,7 @@ class MyApp extends StatelessWidget {
           title: const Text("Welcome to my first Flutter app"),
         ),
         body: Center(
-          child: Text(wordPair.asPascalCase)
+          child: RandomWords(),
           ),
       ),
     );
@@ -25,17 +24,43 @@ class MyApp extends StatelessWidget {
 
 
 class RandomWords extends StatefulWidget {
-  const RandomWords({ Key? key }) : super(key: key);
-
   @override
   _RandomWordsState createState() => _RandomWordsState();
 }
 
 class _RandomWordsState extends State<RandomWords> {
+  final _suggestions = <WordPair>[]; // list view widget
+  final _biggerFont = const TextStyle(fontSize: 18.0);
+ Widget _buildSuggestions() {
+  return ListView.builder(
+      padding: const EdgeInsets.all(16.0),
+      itemBuilder: /*1*/ (context, i) {
+        if (i.isOdd) return const Divider(); /*2*/
+
+        final index = i ~/ 2; /*3*/
+        if (index >= _suggestions.length) {
+          _suggestions.addAll(generateWordPairs().take(10)); /*4*/
+        }
+        return _buildRow(_suggestions[index]);
+      });
+}
+Widget _buildRow(WordPair pair) {
+  return ListTile(
+    title: Text(
+      pair.asPascalCase,
+      style: _biggerFont,
+    ),
+  );
+}
+ 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Name Generator'),
+      ),
+      body: _buildSuggestions(),
     );
+
   }
 }
