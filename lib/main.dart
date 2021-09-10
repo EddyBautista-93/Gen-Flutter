1e849fe14b018c518b1b9e39b8e1d2bfa928b68c
EddyBautista <bautista.j.eddy@gmail.com>
2021-09-10 05:45:57 -0500

    add random word gen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
 // runApp - inflate the given widget and add it to the screen
 void main() => runApp(MyApp());
@@ -7,13 +8,16 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // This class creates the instance of a widget app
+    final wordPair = WordPair.random(); // set the random word var at the top 
     return MaterialApp(
       title: "Welcome to Flutter", // identify the app for the user 
       home: Scaffold( // visual scaffold
         appBar: AppBar(
           title: const Text("Welcome to my first Flutter app"),
         ),
-        body: const Center(child: Text("Hello World")),
+        body: Center(
+          child: Text(wordPair.asPascalCase)
+          ),
       ),
     );
   }
